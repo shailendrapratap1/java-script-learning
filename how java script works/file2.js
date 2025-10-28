8a6acb7ff4a0b985b34034b62376c8f834900c05
Shailendra pratap singh <singhlalprat@gmail.com>
2025-10-28 22:46:17 +0530

    Add files via upload

---
+++ how java script works/file2.js
@@ -0,0 +1,35 @@
+//  //hoisting
+ 
+//  console.log(this);
+// console.log(window);
+// // console.log(firstName);
+// console.log(myFunction);
+// myFunction();
+// // var firstName = "Harshit";
+// console.log(fullName);
+  
+  
+//   var myFunction=function(){
+//     console.log("this is my  function");
+// }
+// var firstName = "harshit";
+// var lastName = "sharma";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+
+// console.log(myFunction);
+
+// var myFunction = function(){
+//     console.log("this is my function");
+// }
+// console.log(myFunction);
+
+
+// console.log(firstName);
+// let firstName = "harshit";
+// console.log(firstName)
+
+console.log(typeof firstName);
+let firstName = "harshit";

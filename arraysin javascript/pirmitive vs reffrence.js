dfa1d18eda405860d2e03f9b6ce9824060a3a141
shailendra pratap <singhlalprat@gmail.com>
2026-01-20 15:41:25 +0530

    Update pirmitive vs reffrence.js

--- arraysin javascript/pirmitive vs reffrence.js
+++ arraysin javascript/pirmitive vs reffrence.js
@@ -1,21 +1,21 @@
 // permitive vs reference data types
-let num1 = 6;
+let num1 =6;
 let num2 = num1;
-console.log("value is num1 is", num1);
-console.log("value is num2 is", num2);
+console.log("value of num1 is", num1);
+console.log("value of num2 is",num2);
 num1++;
-console.log("after incrementing num1")
-console.log("value is num1 is", num1);
-console.log("value is num2 is", num2);
+console.log("after increment num1", num1);
+console.log("value of num1 is ", num1);
+console.log("value of num2 is ",num2);
+
 
 // reference types
 // array 
-let array1 =["item1", "item2"]
-    let array2 = array1;
-    console.log("array1",array1);
-    console.log("array1",array2);
-    array1.push("item3");
- console.log("after pushing to array 1");
- console.log("array1",array1);
-        console.log("array1",array2);
-    
+let array1 = ["item1","item2"];
+let array2 = array1;
+console.log("array1",array1);
+console.log("aray2",array2);
+array1.push("item3");
+console.log("after pushing element to array 1");
+console.log("array1",array1);
+console.log("array2",array2);

f7172df8c27325ee370691c02f427423b10896b2
shailendra pratap <singhlalprat@gmail.com>
2026-01-20 15:25:41 +0530

    Update breakkeyw.js

--- breakkeyw.js
+++ breakkeyw.js
@@ -1,17 +1,12 @@
-// break keywork
-
-// continue keywork 
-
-// for(let i=1;i<=10; i++){
-//     if(i===4){
-//         break;
-//     }
-//     console.log(i);
-// }
-// for(let i=1;i<=10; i++){
-//     if(i===4){
-//         continue;
-//     }
-//     console.log(i);
-// }
-// console.log("hello there");
+for(let i=1;i<=10;i++){
+    if(i==4){
+        break;
+    }
+    console.log(i)
+}
+for(let i=1;i=10;i++){
+    if(i==4){
+        continue;
+    }
+    console.log(i);
+}

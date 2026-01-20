419eb64b139d10a47bb9152ec66cd760df362ca0
shailendra pratap <singhlalprat@gmail.com>
2026-01-20 15:46:22 +0530

    Update clone array.js

--- arraysin javascript/clone array.js
+++ arraysin javascript/clone array.js
@@ -2,15 +2,14 @@
 
 // how to concatence array
 
-let array1 =["item1","item2"];
-// let array2 = ["item1","item2"];
-// let array2 = array1.slice(0).concat(["item3","item4"]);
-// let array2 =[].concat(array1);
-// new way
-// spread opearator
-let oneMoreArray = ["item3","item4"];
-let array2 = [...array1,...oneMoreArray];
+let array1 = ["item1","item2"];
+ array2 = ["item1", "item2"];
+//  array2 = array1.slice(0).concat(["item3","item4"]);
+// let array2 = [].concat(array1,["item3"])
 
+
+let oneMoreArray = ["item3","item4"];
+let array2 = [...array1,...oneMoreArray]
 array1.push("item3");
 
 console.log(array1===array2);

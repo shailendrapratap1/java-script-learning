c178001866c54ed1de5368b834a231570dd99c2e
shailendra pratap <singhlalprat@gmail.com>
2026-01-20 15:34:55 +0530

    Update pushpopshiftunshift.js

--- arraysin javascript/pushpopshiftunshift.js
+++ arraysin javascript/pushpopshiftunshift.js
@@ -1,23 +1,20 @@
-// array push pop 
-
-// array shift unshift v
-
-let fruits =["apple","mango","grapes"];
+let fruits = ["apple","mango","grapes","Banana"];
 console.log(fruits);
-// push
-// fruits.push("banana");
-// console.log(fruits);
-// let poppedFruit = fruits.pop();
-// console.log(fruits);
-// console.log("popped fruits is",poppedFruit);
+fruits.push("banana");
+console.log(fruits)
+//pop
+
+let poppedFruit = fruits.pop();
+console.log(fruits);
+console.log("popped fruits is", poppedFruit);
 
 // unshift 
-// fruits.unshift("banana");
-// fruits.unshift("myfruit");
-// console.log(fruits);
+fruits.unshift("banana");
+fruits.unshift("myFruit");
+console.log(fruits)
 
 // shift 
-// let removedFruit = fruits.shift();
-// console.log(fruits);
-// console.log("removed fruits is",removedFruit);
+let removedFruit = fruits.shift();
+console.log(fruits);
+console.log("removed fruit is", removedFruit);
 

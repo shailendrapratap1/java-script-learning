e78a30e6e4334f4bf8b09ad26f08bdf1cf29c3bd
shailendra pratap <singhlalprat@gmail.com>
2026-01-20 15:26:00 +0530

    Update newkeyword.js

--- oops-javascript/newkeyword.js
+++ oops-javascript/newkeyword.js
@@ -1,34 +1,32 @@
-<<<<<<< HEAD
-// new keyword
-function createUser(firstName, age){
-    this.firstName = firstName;
-    this.age = age;
-}
-createUser.prototype.about = function(){
-    console.log(this.firstName,this.age);
-}
-const user1 = new createUser("harshit",6);
 
-// new keyword
-// 1).empty object this={}
-// 2) return this
-// Object.create(createUser.prototype);
+// // new keyword
+// function createUser(firstName, age){
+//     this.firstName = firstName;
+//     this.age = age;
+// }
+// createUser.prototype.about = function(){
+//     console.log(this.firstName,this.age);
+// }
+// const user1 = new createUser("harshit",6);
 
-=======
-// new keyword
-function createUser(firstName, age){
-    this.firstName = firstName;
-    this.age = age;
-}
-createUser.prototype.about = function(){
-    console.log(this.firstName,this.age);
-}
-const user1 = new createUser("harshit",6);
+// // new keyword
+// // 1).empty object this={}
+// // 2) return this
+// // Object.create(createUser.prototype
+// // new keyword
+// function createUser(firstName, age){
+//     this.firstName = firstName;
+//     this.age = age;
+// }
+// createUser.prototype.about = function(){
+//     console.log(this.firstName,this.age);
+// }
+// const user1 = new createUser("harshit",6);
 
-// new keyword
-// 1).empty object this={}
-// 2) return this
-// Object.create(createUser.prototype);
+// // new keyword
+// // 1).empty object this={}
+// // 2) return this
+// // Object.create(createUser.prototype);
 
->>>>>>> 8a6acb7ff4a0b985b34034b62376c8f834900c05
-user1.about();
+// >>>>>>> 8a6acb7ff4a0b985b34034b62376c8f834900c05
+// user1.about();

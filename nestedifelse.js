3aaccb7d8db084f2cdbea8d8fb8c8d9f39ca761c
shailendra pratap <singhlalprat@gmail.com>
2026-02-06 14:18:05 +0530

    Update nestedifelse.js

--- nestedifelse.js
+++ nestedifelse.js
@@ -489,3 +489,17 @@
 // func();
 
 
+// const user1 = {
+//     firstName:"harshit",
+//     age:8,
+//     about: function(){
+//         console.log(this.firstName,this.age)
+//     }
+
+// }
+// const myFunc = user1.about.bind(user1);
+// myFunc()
+
+function
+
+

709c7364bfd9d4530451f872890a94e3c8736ee6
shailendra pratap <singhlalprat@gmail.com>
2026-02-10 18:28:45 +0530

    live web

--- nestedifelse.js
+++ nestedifelse.js
@@ -639,44 +639,135 @@
 // const func = about.bind(user2,"guitar","bach")
 // func();
 
-const user1 = {
-    firstName:"harshit",
-    age:8,
-    about:()=>{
-        console.log(this.firstName,this.age)
+// const user1 = {
+//     firstName:"harshit",
+//     age:8,
+//     about:()=>{
+//         console.log(this.firstName,this.age)
+//     }
+// }
+
+// user1.about();
+
+
+
+// function createUser(firstName,lastName,email,age,address){
+//     const user = {};
+//     user.firstName = firstName;
+//     user.lastName=lastName;
+//     user.email = email;
+//     user.age=age;
+//     user.address = address;
+//     user.about = function(){
+//         return`${this.firstName} is ${this.age} years old`;
+//     }
+//     user.is18 = function(){
+//         return this.age>=18
+//     }
+//    return user
+// }
+// const user1 = createUser('harshit','vashisth','harshit@gmail.com',19,"my address")
+
+// console.log(user1);
+// const is18 = user1.is18();
+// const about = user1.about();
+// console.log(about)
+
+
+
+
+
+
+
+
+
+// const userMethods = {
+//     about : function(){
+//         return `${this.firstName} is ${this.age} years old.`;
+//     },
+//     is18 : function(){
+//         return this.age >= 18;
+//     },
+//     sing: function(){
+//         return 'toon na na na la la ';
+//     }
+// }
+// function createUser(firstName, lastName, email, age, address){
+//     const user = Object.create(createUser.prototype);// {}
+//     user.firstName = firstName;
+//     user.lastName = lastName;
+//     user.email = email;
+//     user.age = age;
+//     user.address = address;
+//     return user;
+// }
+// createUser.prototype.about = function(){
+//     return `${this.firstName} is ${this.age} years old.`;
+// };
+// createUser.prototype.is18 = function (){
+//     return this.age >= 18; 
+// }
+// createUser.prototype.sing = function (){
+//     return "la la la la ";
+// }
+
+
+// const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
+// const user2 = createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+// const user3 = createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+// console.log(user1);
+// console.log(user1.is18());
+
+
+
+// class Animal{
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     eat(){
+//         return`${this.name} is eating`
+//     }
+//     isCute(){
+//         return true 
+//     }
+// }
+//   class Dog extends Animal{
+        
+//     }
+//     const tommy = new Dog("tommy",3)
+// console.log(tommy)
+// console.log(tommy.isCute)
+
+
+class Animal {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    eat(){
+        return `${this.name} is eating`;
+    }
+
+    isSuperCute(){
+        return this.age <= 1;
+    }
+
+    isCute(){
+        return true;
     }
 }
 
-user1.about();
+class Dog extends Animal{
+    constructor(name, age, speed){
+        super(name,age);
+        this.speed = speed;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    run(){
+        return `${this.name} is running at ${this.speed}kmph`
+    }
+} 
+const tommy = new Dog("tommy", 3,45);
+console.log(tommy.run());

71a42e31dc36bc0906f6326d26e57a98291c4abb
shailendra pratap <singhlalprat@gmail.com>
2026-02-13 15:52:34 +0530

    Update 104.js

--- DOM TUTORIAL/104.js
+++ DOM TUTORIAL/104.js
@@ -1,6 +1,10 @@
 // change text 
 // text-content and innertext 
 const mainHeading = document.getElementById("main-heading");
+console.log(mainHeading.innerText);
+mainHeading.textContent = " This is something else"
+console.log(mainHeading.textContent);
+// const mainHeading = document.getElementById("main-heading");
 // console.log(mainHeading.innerText);
-// mainHeading.textContent = " This is something else"
-// console.log(mainHeading.textContent);
+// mainHeading.textContent = "this is something else"
+// console.log(mainHeading.textContent)

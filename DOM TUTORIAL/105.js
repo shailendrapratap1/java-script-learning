2d3a161bd2a243339030cb381ed4c06823cbd0dc
shailendra pratap <singhlalprat@gmail.com>
2026-02-13 15:52:43 +0530

    Update 105.js

--- DOM TUTORIAL/105.js
+++ DOM TUTORIAL/105.js
@@ -1,5 +1,11 @@
 // change the style of element
-const mainHeading = document.querySelector("div.headline h2");
-// console.log(mainHeading.style);
-mainHeading.style.backgroundColor = "blue";
-mainHeading.style.border = "2px solid green"
+// const mainHeading = document.querySelector("div.headline h2");
+// // console.log(mainHeading.style);
+// mainHeading.style.backgroundColor = "blue";
+// mainHeading.style.border = "2px solid green"
+
+const mainHeading = document.querySelector("div.headline h2")
+console.log(mainHeading.style)
+mainHeading.style.backgroundColor = "green"
+mainHeading.style.border ="4px solid black"
+

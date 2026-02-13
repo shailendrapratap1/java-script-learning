c84b0cfc08b83ea81733b7b596c6e66f697a5256
shailendra pratap <singhlalprat@gmail.com>
2026-02-13 15:52:29 +0530

    Update nestedifelse.js

--- nestedifelse.js
+++ nestedifelse.js
@@ -796,43 +796,43 @@
 // console.log(user1)
 // console.log(user1.is18())
 
-class Person{
-   constructor(firstName,lastName,age){
-    this.firstName=firstName;
-    this.lastName = lastName;
-    this.age = age;
+// class Person{
+//    constructor(firstName,lastName,age){
+//     this.firstName=firstName;
+//     this.lastName = lastName;
+//     this.age = age;
     
-   }
-   static classInfo(){
-    return 'this is person class'
-   }
-   static desc = 'static property'
-   get fullName(){
-    return `${this.firstName} ${this.lastName}`
-   }
-   set fullName(fullName){
-    const [firstName,lastName] = fullName.split(" ");
-    this.firstName = firstName;
-    this.lastName = lastName;
+//    }
+//    static classInfo(){
+//     return 'this is person class'
+//    }
+//    static desc = 'static property'
+//    get fullName(){
+//     return `${this.firstName} ${this.lastName}`
+//    }
+//    set fullName(fullName){
+//     const [firstName,lastName] = fullName.split(" ");
+//     this.firstName = firstName;
+//     this.lastName = lastName;
     
-   }
-   eat(){
-    return `${this.firstName} is eating`
-   }
-   isSuperCute(){
-    return this.age <= 1;
-   }
-   isCute(){
-    return true;
-   }
-}
-const person1 = new Person("harshit", "sharma", 8);
+//    }
+//    eat(){
+//     return `${this.firstName} is eating`
+//    }
+//    isSuperCute(){
+//     return this.age <= 1;
+//    }
+//    isCute(){
+//     return true;
+//    }
+// }
+// const person1 = new Person("harshit", "sharma", 8);
 
-console.log(person1.eat())
-  const info = Person.classInfo();
+// console.log(person1.eat())
+//   const info = Person.classInfo();
 
-console.log(person1.desc)
-console.log(info)
+// console.log(person1.desc)
+// console.log(info)
 
 
 

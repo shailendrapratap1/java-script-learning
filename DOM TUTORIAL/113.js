d8acdc8b12a101b765688561dfae83dcf0b3b52b
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 13:19:14 +0530

    Update 113.js

--- DOM TUTORIAL/113.js
+++ DOM TUTORIAL/113.js
@@ -7,7 +7,8 @@ const todoList = document.querySelector(".todo-list");
 // console.log(todoList.innerHtml);
 // todoList.innerHTML = "<li>New Todo</li>";
 // todoList.innerHTML += "<li>teach Todo</li>";
-
+ const headeer = document.querySelector(".header")
+header.classList.add("bg-dark")
 
 // when you should use it , when you should not
 

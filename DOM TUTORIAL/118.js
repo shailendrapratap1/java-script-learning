2a00da1c80f8d609bb0ff6ebc2c9ead2b1d89498
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 18:40:31 +0530

    Update 118.js

--- DOM TUTORIAL/118.js
+++ DOM TUTORIAL/118.js
@@ -10,3 +10,10 @@ sixthLi.textContent ="item6";
 // const ul = document.querySelector(".todo-list");
 ul.append(sixthLi);
 console.log(listItems);
+
+// const ul = document.querySelector(".todo-list");
+// const listItems = ul.getElementsByTagName("li");
+// const sixthLi = document.createElement("li")
+// sixthLi.textContent ="item6"
+// ul.append(sixthLi)
+// console.log(listItems)

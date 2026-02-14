c91ce0b62356b7fae7617b8430b0b86c8970b276
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 18:40:40 +0530

    Update 120.js

--- DOM TUTORIAL/120.js
+++ DOM TUTORIAL/120.js
@@ -3,18 +3,24 @@
 // 1.)
 const btn = document.querySelector(".btn-headline");
 // method --> add event listner 
+function clickMe(){
+    console.log("you clicked me ")
+}
+btn.addEventListener("click",function(){
+    console.log("you clicked me");
+});
+
+
+btn.addEventListener("click",()=>{
+    console.log("you clicked me");
+});
+// const btn = document.querySelector(".btn-headiline")
 // function clickMe(){
-//     console.log("you clicked me ")
+//     console.log("you clicked me")
 // }
 // btn.addEventListener("click",function(){
-//     console.log("you clicked me");
-// });
-
-
-// btn.addEventListener("click",()=>{
-//     console.log("you clicked me");
-// });
-
+//     console.log("you clicked me")
+// })
 
 
 

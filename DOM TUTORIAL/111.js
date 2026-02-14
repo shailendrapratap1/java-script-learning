253dda872d051a69aefa0155ab9c84dce0291c92
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 13:19:09 +0530

    Update 111.js

--- DOM TUTORIAL/111.js
+++ DOM TUTORIAL/111.js
@@ -1,9 +1,10 @@
-// const rootNode = document.getRootNode();
-// const htmlElementNode = rootNode.childNodes[0];
-// // console.log(htmlElementNode.childNodes);  NodeList(3) [head, text, body]
-// const headElementNode = htmlElementNode.childNodes[0];
-// const textNode1 = htmlElementNode.childNodes[1];
-// const bodyElementNode = htmlElementNode.childNodes[2];
+const rootNode = document.getRootNode();
+const htmlElementNode = rootNode.childNodes[0];
+// console.log(htmlElementNode.childNodes);
+//   NodeList(3) [head, text, body]
+const headElementNode = htmlElementNode.childNodes[0];
+const textNode1 = htmlElementNode.childNodes[1];
+const bodyElementNode = htmlElementNode.childNodes[2];
 // console.log(headElementNode.childNodes);
 // sibling relation
 
@@ -20,7 +21,39 @@
 // // console.log(head);
 // const title = head.querySelector("title");
 // console.log(title.childNodes);
-const container = document.querySelector(".container");
-console.log(container.children);
+// const container = document.querySelector(".container");
+// console.log(container.children);
+
+
+// const rootNode =  document.getRootNode();
+// const htmlElementNode = rootNode.childNodes[0];
+// console.log(htmlElementNode.childNodes)
+// const headElementNode =  htmlElementNode.childNodes[0]
+// const textNode1 = htmlElementNode.childNodes[1];
+// const bodyElementNode = htmlElementNode.childNodes[2];
+// console.log(headElementNode.childNodes)
+
+
+// const h1 =  document.querySelector("h1");
+// const body = h1.parentNode.parentNode;
+// body.style.color = "#efefef"
+// body.style.backgroundColor = "#3333"
+
+
+const sectionTodo = document.querySelector(".section-todo");
+console.log(sectionTodo.classList)
+sectionTodo.classList.add('bg-green')
+
+
+
+
+
+
+
+
+
+
+
+
 
 

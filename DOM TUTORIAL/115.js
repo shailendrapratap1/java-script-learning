f050d3e0b61529f2f3a067678cf84d01de42267e
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 13:19:27 +0530

    Update 115.js

--- DOM TUTORIAL/115.js
+++ DOM TUTORIAL/115.js
@@ -4,5 +4,21 @@
 // beforeend
 // afterend
 
-// const todoList = document.querySelector(".todo-list");
-// todoList.insertAdjacentHTML("afterend","<li>Teach Students</li>");
+const todoList = document.querySelector(".todo-list");
+todoList.insertAdjacentHTML("afterbegin","<li>Teach Students</li>");
+
+
+todoList.insertAdjacentHTML("afterbegin","<li>the lisy is clear</li>")
+
+
+
+
+
+
+
+
+
+
+
+
+

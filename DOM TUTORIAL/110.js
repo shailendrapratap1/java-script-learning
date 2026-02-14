7ece250329a17c27b169b1669f7148bd3bbbd481
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 13:19:06 +0530

    Update 110.js

---
+++ DOM TUTORIAL/110.js
@@ -0,0 +1,7 @@
+const headline = document.querySelector(".headline");
+console.log(headline.innerHTML)
+headline.innerHTML = "<h1>Inner Html changed</h1>"
+headline.innerHTML  += "<button class=\"btn\"> Learn More </button>"
+
+
+

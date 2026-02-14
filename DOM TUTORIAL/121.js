52d446b8947b9898d795520f29ca975bd6108d15
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 18:40:45 +0530

    Update 121.js

--- DOM TUTORIAL/121.js
+++ DOM TUTORIAL/121.js
@@ -1,12 +1,17 @@
 // this keyword 
-const btn = document.querySelector(".btn-headline");
- btn.addEventListener("click",()=>{
+// const btn = document.querySelector(".btn-headline");
+//  btn.addEventListener("click",()=>{
+//     console.log("you clicked me");
+//     console.log("value of this");
+//     console.log(this);
+// });
+
+const btn = document.querySelector(".btn-headline")
+btn.addEventListener("click",()=>{
     console.log("you clicked me");
-    console.log("value of this");
-    console.log(this);
-});
-
-
+    console.log("value of this")
+    console.log(this)
+})
 // normal vs arrow different results will occur 
 // arrow always will one step ahead
 

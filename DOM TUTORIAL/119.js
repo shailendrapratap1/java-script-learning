f1a91e6d1b607b779b811c6c6bed8fe1ce4e1d51
shailendra pratap <singhlalprat@gmail.com>
2026-02-14 18:40:36 +0530

    Update 119.js

--- DOM TUTORIAL/119.js
+++ DOM TUTORIAL/119.js
@@ -1,6 +1,10 @@
 // how to get the dimensions of an element
 // height width
 
+// const sectionTodo = document.querySelector(".section-todo");
+// const info = sectionTodo.getBoundingClientRect().height;
+// console.log(info);
+
 const sectionTodo = document.querySelector(".section-todo");
-const info = sectionTodo.getBoundingClientRect().height;
-console.log(info);
+const info  =  sectionTodo.getBoundingClientRect().height;
+console.log(info)

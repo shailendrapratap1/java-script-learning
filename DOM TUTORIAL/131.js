b13670249798056eb4e6e1b8db13b85fd72fb4f4
shailendra pratap <singhlalprat@gmail.com>
2026-02-17 18:26:43 +0530

    Update 131.js

--- DOM TUTORIAL/131.js
+++ DOM TUTORIAL/131.js
@@ -7,17 +7,33 @@
 // //     console.log(Math.random());
 // //  },500);
 //  console.log("script end");
+// const body = document.body;
+// const button = document.querySelector("button");
+// const intervalId=setInterval(()=>{
+//     const red =Math.floor(Math.random() * 126);
+//         const green =Math.floor(Math.random() * 126);
+//     const blue=Math.floor(Math.random() * 126);
+//    const rgb =`rgb(${red},${green},${blue})`
+//     body.style.background=rgb;
+// },1000);
+// button.addEventListener("click",()=>{
+//     clearInterval(intervalId);
+//     button.textContent = body.style.background;
+// })
+// console.log(intervalId);
 const body = document.body;
 const button = document.querySelector("button");
-const intervalId=setInterval(()=>{
-    const red =Math.floor(Math.random() * 126);
-        const green =Math.floor(Math.random() * 126);
-    const blue=Math.floor(Math.random() * 126);
-   const rgb =`rgb(${red},${green},${blue})`
-    body.style.background=rgb;
-},1000);
+const intervalId = setInterval(()=>{
+  const red = Math.floor(Math.random()*256);
+  const green = Math.random(Math.floor()*256)
+  const blue = Math.random(Math.floor()*256);
+  const rgb = `rgb(${red},${green},${blue})`
+  body.style.background = rgb;
+
+},1000)
 button.addEventListener("click",()=>{
-    clearInterval(intervalId);
+    clearInterval(intervalId)
     button.textContent = body.style.background;
 })
-console.log(intervalId);
+
+console.log(intervalId)

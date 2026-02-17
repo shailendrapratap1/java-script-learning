bfa5e1800bcb99c8d3c741f0f8adbcff3c63f245
shailendra pratap <singhlalprat@gmail.com>
2026-02-17 18:26:49 +0530

    Update 133.js

--- DOM TUTORIAL/133.js
+++ DOM TUTORIAL/133.js
@@ -1,81 +1,112 @@
-// callbacks , callback hell, pyramid of doom
-// asynchronous programming 
+// // callbacks , callback hell, pyramid of doom
+// // asynchronous programming 
+// // const heading1 = document.querySelector(".heading1");
+// // const heading2 = document.querySelector(".heading2");
+// // setTimeout(()=>{
+// //     heading1.textContent ="Heading 1 "
+// //     heading1.style.color ="violet";
+// //     setTimeout(()=>{
+// //         heading2.textContent ="Heading 2"
+// //     heading2.style.color ="purple";
+// //     },1000)
+// // },1000);
+// // callbacks , callback hell, pyramid of doom
+// // asynchronous programming
 // const heading1 = document.querySelector(".heading1");
 // const heading2 = document.querySelector(".heading2");
-// setTimeout(()=>{
-//     heading1.textContent ="Heading 1 "
-//     heading1.style.color ="violet";
-//     setTimeout(()=>{
-//         heading2.textContent ="Heading 2"
-//     heading2.style.color ="purple";
-//     },1000)
-// },1000);
-// callbacks , callback hell, pyramid of doom
-// asynchronous programming
-const heading1 = document.querySelector(".heading1");
-const heading2 = document.querySelector(".heading2");
-const heading3 = document.querySelector(".heading3");
-const heading4 = document.querySelector(".heading4");
-const heading5 = document.querySelector(".heading5");
-const heading6 = document.querySelector(".heading6");
-const heading7 = document.querySelector(".heading7");
-const heading8 = document.querySelector(".heading8");
-const heading9 = document.querySelector(".heading9");
-const heading10 = document.querySelector(".heading10");
+// const heading3 = document.querySelector(".heading3");
+// const heading4 = document.querySelector(".heading4");
+// const heading5 = document.querySelector(".heading5");
+// const heading6 = document.querySelector(".heading6");
+// const heading7 = document.querySelector(".heading7");
+// const heading8 = document.querySelector(".heading8");
+// const heading9 = document.querySelector(".heading9");
+// const heading10 = document.querySelector(".heading10");
 
-// Text       Delay   Color
+// // Text       Delay   Color
 
-// one        1s      Violet
-// two        2s      purple
-// three      2s      red
-// four       1s      Pink
-// five       2s      green
-// six        3s      blue
-// seven      1s      brown
+// // one        1s      Violet
+// // two        2s      purple
+// // three      2s      red
+// // four       1s      Pink
+// // five       2s      green
+// // // six        3s      blue
+// // // seven      1s      brown
 
-// callback hell  
-// setTimeout(()=>{
-//   heading1.textContent = "one";
-//   heading1.style.color = "violet";
-//   setTimeout(()=>{
-//     heading2.textContent = "two";
-//     heading2.style.color = "purple";
-//     setTimeout(()=>{
-//       heading3.textContent = "three";
-//       heading3.style.color = "red";
-//       setTimeout(()=>{
-//         heading4.textContent = "four";
-//         heading4.style.color = "pink";
-//         setTimeout(()=>{
-//           heading5.textContent = "five";
-//           heading5.style.color = "green";
-//         },2000)
+// // // callback hell  
+// // // setTimeout(()=>{
+// // //   heading1.textContent = "one";
+// // //   heading1.style.color = "violet";
+// // //   setTimeout(()=>{
+// // //     heading2.textContent = "two";
+// // //     heading2.style.color = "purple";
+// // //     setTimeout(()=>{
+// // //       heading3.textContent = "three";
+// // //       heading3.style.color = "red";
+// // //       setTimeout(()=>{
+// // //         heading4.textContent = "four";
+// // //         heading4.style.color = "pink";
+// // //         setTimeout(()=>{
+// // //           heading5.textContent = "five";
+// // //           heading5.style.color = "green";
+// // //         },2000)
         
-//       },1000)
+// // //       },1000)
       
-//     },2000)
+// // //     },2000)
     
-//   },2000)
+// // //   },2000)
   
-// },1000)
-function changeText(element, text, color,time,onSuccessCallback, onFaliureCallback){
-    setTimeout(()=>{
-        if(element){  element.textContent = text;
-        element.style.color = color;}
-        if(onSuccessCallback){
-            onSuccessCallback();
-        }else{
-            if(onFaliureCallback){
-                onFaliureCallback();
-            }
-        }   
-    },time)
-}
+// // // },1000)
+// // function changeText(element, text, color,time,onSuccessCallback, onFaliureCallback){
+// //     setTimeout(()=>{
+// //         if(element){  element.textContent = text;
+// //         element.style.color = color;}
+// //         if(onSuccessCallback){
+// //             onSuccessCallback();
+// //         }else{
+// //             if(onFaliureCallback){
+// //                 onFaliureCallback();
+// //             }
+// //         }   
+// //     },time)
+// // }
 
-changeText(heading1,"one","green",1000,()=>{
-changeText(heading2,"teo","green",2000,()=>{
+// // changeText(heading1,"one","green",1000,()=>{
+// // changeText(heading2,"teo","green",2000,()=>{
 
-},()=>{
-   console.log("your element does not exist") 
-})},()=>{
-   console.log("your element does not exist")});
+// // },()=>{
+// //    console.log("your element does not exist") 
+// // })},()=>{
+// //    console.log("your element does not exist")});
+
+
+// function changeText (element,text,color,time,onSuccessCallback,onFaliureCallback){
+//     setTimeout(()=>{
+//         if(element){
+//             element.textContent = text;
+//             element.style.color = color;
+//             if(onSuccessCallback){
+//                 onSuccessCallback();
+//             }else{
+//                 if(onFaliureCallback){
+//                     onFaliureCallback();
+//                 }
+//             }
+//         }
+//     },time)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

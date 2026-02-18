3ab648710730b2019747fab9ab80795fbb30df1a
shailendra pratap <singhlalprat@gmail.com>
2026-02-18 18:02:12 +0530

    Create 136.js

---
+++ DOM TUTORIAL/136.js
@@ -0,0 +1,22 @@
+// promise and ssetTimeOut
+
+
+// i want to resolve or reject my promise afer 2 seconds
+
+
+function myPromise(){
+    return new Promise((resolve,reject)=>{
+        const value = true;
+        setTimeout(()=>{
+            if(value){
+                resolve();
+            }else{
+                reject();
+            }
+        },2000)
+    })
+}
+myPromise().then(()=>{console.log("resolved")})
+.catch(()=>{console.log("rejected")})
+
+
